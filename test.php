73d2d0f28eddcc3c9161472ded8037aa3468ea67
luntikius <luntikius@gmail.com>
2024-03-04 15:38:06 +0300

    - added bobik to test.php

--- test.php
+++ test.php
@@ -5,6 +5,19 @@ $arr = array(1,"bobik",190,"\n");
 foreach ($arr as $element){
     echo $element;
 }
+echo($arr[0]);
 
-echo($arr[0])
+class Bobik{
+    public function bark()
+    {
+        echo "BARK\n";
+    }
+}
+
+$dog = new Bobik();
+
+$dog -> bark();
+$dog -> bark();
+$dog -> bark();
+
 ?>
